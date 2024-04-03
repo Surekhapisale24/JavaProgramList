5f3e6ebba21f6ecd7699d7e469b3d2e4acea79f8
Surekhapisale24 <139945281+Surekhapisale24@users.noreply.github.com>
2024-04-03 22:52:17 +0530

    Add files via upload
    Unique chars file uploaded today

---
+++ src/strings/com/UniqueCharsFrmString.java
@@ -0,0 +1,62 @@
+package strings.com;
+import java.util.*;
+public class UniqueCharsFrmString 
+{
+	public static void main(String[] args)
+	{
+		System.out.print("Unique chars from given Strings are: ");
+		uniqueChars("ccrert");
+		
+		
+		System.out.print("Duplictate chars from given Strings are: ");
+		duplicateChars("ccrert");
+	}
+	
+	public static void uniqueChars(String str)
+	{
+		char charArray[] = str.toCharArray();
+		int count;
+		
+		for(int i = 0; i < str.length(); i++)
+		{
+			count = 1;
+			for(int j = i+1; j <str.length(); j++)
+			{
+				if(charArray[i] == charArray[j] && charArray[i] != ' ')
+				{
+					count++;
+				}
+			}
+			
+			if(count == 1)
+			{
+				System.out.print(charArray[i]);
+			}
+		}
+		System.out.println();
+	}
+	
+		
+		public static void duplicateChars(String str)
+		{
+			char charArray[] = str.toCharArray();
+			int count;
+			
+			for(int i = 0; i < str.length(); i++)
+			{
+				count = 1;
+				for(int j = i+1; j <str.length(); j++)
+				{
+					if(charArray[i] == charArray[j] && charArray[i] != ' ')
+					{
+						count++;
+					}
+				}
+				
+				if(count > 1)
+				{
+					System.out.print(charArray[i]);
+				}
+			}
+	}
+}
