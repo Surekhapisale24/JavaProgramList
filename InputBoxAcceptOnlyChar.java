3d8dffe6161a81df6e753f8e8c5071f0a583507d
Surekhapisale24 <139945281+Surekhapisale24@users.noreply.github.com>
2024-04-04 19:08:19 +0530

    Add files via upload
    commiting String 4/4/24

---
+++ InputBoxAcceptOnlyChar.java
@@ -0,0 +1,34 @@
+package strings.com;
+import java.util.*;
+public class InputBoxAcceptOnlyChar
+{
+	public static void main(String[] args) 
+	{
+		//System.out.println("Hello World");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("take user input");
+		String inputStr = sc.nextLine();
+		boolean isValid = validateString(inputStr);
+		
+		if(isValid)
+		{
+		    System.out.println("String is valid");
+		}
+		else
+		{
+		    System.out.println("Stirng is not valid");
+		}
+	}
+	
+	public static boolean validateString(String str)
+	{
+	    for(char c : str.toCharArray())
+	    {
+	        if(!Character.isLetter(c) && !Character.isWhitespace(c))
+	        {
+	            return false;
+	        }
+	    }
+	    return true;
+	}
+}
